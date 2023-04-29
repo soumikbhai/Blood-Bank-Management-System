ce0ec738986efe19c2de7d819082597f03daf98e
soumikbhai <soumik.roy@g.bracu.ac.bd>
2023-04-29 22:50:09 +0600

    donatetheblood

---
+++ about.php
@@ -0,0 +1,108 @@
+<?php 
+
+	//include header file
+	include ('include/header.php');
+
+?>
+
+<style>
+	.size{
+		padding: 80px 0px;
+	}
+	img{
+		width: 300px;
+		height: 300px;
+	}
+	h2{
+		color: #e74c3c;
+	}
+	.white{
+		background-color: white;
+	}
+	p{
+		font-size: 18px;
+	}
+	.right{
+		float: right;
+	}
+	h1{
+		color: white;
+	}
+	.size{
+		min-height: 0px;
+		padding: 60px 0 40px 0;
+		
+	}
+
+</style>
+<div class="container-fluid red-background size">
+	<div class="row">
+		<div class="col-md-6 offset-md-3">
+			<h1 class="text-center">About Us</h1>
+			<hr class="white-bar">
+		</div>
+	</div>
+</div>
+<div class="container-fluid size">
+	
+	<div class="container">
+		<div class="row">
+		<div class="col-md-6"><img src="img/binoculars.png" alt="Our Vission" class="rounded float-left img-fluid"></div>
+		<div class="col-md-6">
+			<h2 class="text-center">Our Vission</h2>
+			<hr class="red-bar">
+			<p>
+				It is said that blood is one of the most priceless gifts one can give to another. Blood is essential for a person to stay alive. Many times due to accidents or any other serious ailments, a person might require blood. And in those times, people who step up to donate their blood are real-life superheroes.
+			</p>
+			<p>
+				It is said that blood is one of the most priceless gifts one can give to another. Blood is essential for a person to stay alive. Many times due to accidents or any other serious ailments, a person might require blood. And in those times, people who step up to donate their blood are real-life superheroes.
+			</p>
+		</div>
+	</div>
+	</div>
+</div>
+
+<div class="container-fluid white size">
+	<div class="container ">
+	<div class="container">
+		<div class="row ">
+		<div class="col-md-6">
+			<h2 class="text-center">Our Goal</h2>
+			<hr class="red-bar">
+			<p>
+				It is said that blood is one of the most priceless gifts one can give to another. Blood is essential for a person to stay alive. Many times due to accidents or any other serious ailments, a person might require blood. And in those times, people who step up to donate their blood are real-life superheroes.
+			</p>
+			<p>
+				It is said that blood is one of the most priceless gifts one can give to another. Blood is essential for a person to stay alive. Many times due to accidents or any other serious ailments, a person might require blood. And in those times, people who step up to donate their blood are real-life superheroes.
+			</p>
+		</div>
+		<div ><img src="img/target.png" alt="Our Vission" class="rounded img-fluid float-right"></div>
+	</div>
+	</div>		
+</div>
+</div>
+	
+
+	<div class="container-fluid size">
+		<div class="container">
+		<div class="row">
+		<div class="col-md-6"><img src="img/goal.png" alt="Our Vission" class="rounded float-left img-fluid"></div>
+		<div class="col-md-6">
+			<h2 class="text-center">Our Mission</h2>
+			<hr class="red-bar">
+			<p>
+				It is said that blood is one of the most priceless gifts one can give to another. Blood is essential for a person to stay alive. Many times due to accidents or any other serious ailments, a person might require blood. And in those times, people who step up to donate their blood are real-life superheroes.
+			</p>
+			<p>
+				It is said that blood is one of the most priceless gifts one can give to another. Blood is essential for a person to stay alive. Many times due to accidents or any other serious ailments, a person might require blood. And in those times, people who step up to donate their blood are real-life superheroes.
+			</p>
+		</div>
+	</div>
+	</div>
+	</div>
+<?php 
+
+	//include footer file
+	include ('include/footer.php');
+
+?>
---
+++ index.php
@@ -0,0 +1,109 @@
+<?php 
+
+	//include header file
+	include ('include/header.php');
+
+?>
+
+
+<div class="container-fluid header-img">
+				<div class="row">
+					<div class="col-md-6 offset-md-3">
+
+						<div class="header">
+							<h1 class="text-center">Don’t be “A negative”; be “O positive”.</h1>
+						<p class="text-center">A single drop of blood can make a huge difference.</p>
+						</div>
+
+
+						<h1 class="text-center">Search The Donors</h1>
+						<hr class="white-bar text-center">
+
+						<form action="search.php" method="get" class="form-inline text-center" style="padding: 40px 0px 0px 5px;">
+							<div class="form-group text-center justify-content-center">
+							
+								<select style="width: 220px; height: 45px;" name="city" id="city" class="form-control demo-default" required>
+	<option value="">-- Select --</option><optgroup title="Dhaka" label="&raquo; Dhaka"></optgroup><option value="Abdullahpur" >Abdullahpur</option><option value="Uttara" >Uttara</option><option value="Pallabi" >Pallabi</option><option value="Mirpur" >Mirpur</option><option value="Kazipara" >Kazipara</option><option value="Kafrul" >Kafrul</option><option value="Agargaon" >Agargaon</option><option value="Sher-e-Bangla Nagar" >Sher-e-Bangla Nagar</option><optgroup title="Chattogram " label="&raquo; Chattogram "></optgroup><option value="Agrabad" >Agrabad</option><option value="Bhatiari" >Bhatiari</option><option value="Chandgoan residential area" >Chandgoan residential area</option><option value="Faujdarhat" >Faujdarhat</option><option value="Halishahar" >Halishahar</option><option value="Patenga" >Patenga</option><option value="North Kattali" >North Kattali</option><option value="South Kattali" >South Kattali</option><option value="Pahartali" >Pahartali</option><option value="Saraipara" >Saraipara</option><option value="Noapara" >Noapara</option><option value="Akbarshah" >Akbarshah</option><option value="Bayazid" >Bayazid</option><option value="Bakalia" >Bakalia</option><option value="Panchlaish" >Panchlaish</option><option value="Chawk Bazar" >Chawk Bazar</option><option value="Mohra" >Mohra</option><option value="Khatunganj" >Khatunganj</option><option value="Patharghata" >Patharghata</option><option value="Bakshirhat" >Bakshirhat</option><option value="Sanowara Residential Area" >Sanowara Residential Area</option><option value="Hamid Char" >Hamid Char</option><option value="Annona Residential Area" >Annona Residential Area</option><option value="Burishchar" >Burishchar</option><option value="Nandan Kanan" >Nandan Kanan</option><option value="Jamal Khan" >Jamal Khan</option><option value="Lalkhan Bazar" >Lalkhan Bazar</option><optgroup title="Federally Administered Tribal Areas (FATA" label="&raquo; Federally Administered Tribal Areas (FATA"></optgroup><option value="Bajaur Agency" >Bajaur Agency</option><option value="Khyber Agency" >Khyber Agency</option><option value="Kurram Agency" >Kurram Agency</option><option value="Mohmand Agency" >Mohmand Agency</option><option value="North Waziristan Agency" >North Waziristan Agency</option><option value="Orakzai Agency" >Orakzai Agency</option><option value="South Waziristan Agency" >South Waziristan Agency</option><optgroup title="Islamabad Capital" label="&raquo; Islamabad Capital"></optgroup><option value="Islamabad" >Islamabad</option><optgroup title="North-West Frontier Province (NWFP)" label="&raquo; North-West Frontier Province (NWFP)"></optgroup><option value="Abbottabad" >Abbottabad</option><option value="Bannu" >Bannu</option><option value="Batagram" >Batagram</option><option value="Buner" >Buner</option><option value="Charsadda" >Charsadda</option><option value="Chitral" >Chitral</option><option value="Dera Ismail Khan" >Dera Ismail Khan</option><option value="Dir Lower" >Dir Lower</option><option value="Dir Upper" >Dir Upper</option><option value="Hangu" >Hangu</option><option value="Haripur" >Haripur</option><option value="Karak" >Karak</option><option value="Kohat" >Kohat</option><option value="Kohistan" >Kohistan</option><option value="Lakki Marwat" >Lakki Marwat</option><option value="Malakand" >Malakand</option><option value="Mansehra" >Mansehra</option><option value="Mardan" >Mardan</option><option value="Nowshera" >Nowshera</option><option value="Peshawar" >Peshawar</option><option value="Shangla" >Shangla</option><option value="Swabi" >Swabi</option><option value="Swat" >Swat</option><option value="Tank" >Tank</option><optgroup title="Punjab" label="&raquo; Punjab"></optgroup><option value="Alipur" >Alipur</option><option value="Attock" >Attock</option><option value="Bahawalnagar" >Bahawalnagar</option><option value="Bahawalpur" >Bahawalpur</option><option value="Bhakkar" >Bhakkar</option><option value="Chakwal" >Chakwal</option><option value="Chiniot" >Chiniot</option><option value="Dera Ghazi Khan" >Dera Ghazi Khan</option><option value="Faisalabad" >Faisalabad</option><option value="Gujranwala" >Gujranwala</option><option value="Gujrat" >Gujrat</option><option value="Hafizabad" >Hafizabad</option><option value="Jhang" >Jhang</option><option value="Jhelum" >Jhelum</option><option value="Kasur" >Kasur</option><option value="Khanewal" >Khanewal</option><option value="Khushab" >Khushab</option><option value="Lahore" >Lahore</option><option value="Layyah" >Layyah</option><option value="Lodhran" >Lodhran</option><option value="Mandi Bahauddin" >Mandi Bahauddin</option><option value="Mianwali" >Mianwali</option><option value="Multan" >Multan</option><option value="Muzaffargarh" >Muzaffargarh</option><option value="Nankana Sahib" >Nankana Sahib</option><option value="Narowal" >Narowal</option><option value="Okara" >Okara</option><option value="Pakpattan" >Pakpattan</option><option value="Rahim Yar Khan" >Rahim Yar Khan</option><option value="Rajanpur" >Rajanpur</option><option value="Rawalpindi" >Rawalpindi</option><option value="Sahiwal" >Sahiwal</option><option value="Sargodha" >Sargodha</option><option value="Sheikhupura" >Sheikhupura</option><option value="Shekhupura" >Shekhupura</option><option value="Sialkot" >Sialkot</option><option value="Toba Tek Singh" >Toba Tek Singh</option><option value="Vehari" >Vehari</option><optgroup title="Sindh" label="&raquo; Sindh"></optgroup><option value="Badin" >Badin</option><option value="Dadu" >Dadu</option><option value="Ghotki" >Ghotki</option><option value="Hyderabad" >Hyderabad</option><option value="Jacobabad" >Jacobabad</option><option value="Jamshoro" >Jamshoro</option><option value="Karachi" >Karachi</option><option value="Kashmore" >Kashmore</option><option value="Khairpur" >Khairpur</option><option value="Larkana" >Larkana</option><option value="Matiari" >Matiari</option><option value="Mirpur Khas" >Mirpur Khas</option><option value="Naushahro Feroze" >Naushahro Feroze</option><option value="Nawabshah" >Nawabshah</option><option value="Qambar Shahdadkot" >Qambar Shahdadkot</option><option value="Sanghar" >Sanghar</option><option value="Shikarpur" >Shikarpur</option><option value="Sukkur" >Sukkur</option><option value="Tando Allahyar" >Tando Allahyar</option><option value="Tando Muhammad Khan" >Tando Muhammad Khan</option><option value="Tharparkar" >Tharparkar</option><option value="Thatta" >Thatta</option><option value="Umerkot" >Umerkot</option></select>
+							</div>
+							<div class="form-group center-aligned">
+								<select name="blood_group" id="blood_group" style="padding: 0 20px; width: 220px; height: 45px;" class="form-control demo-default text-center margin10px">
+									
+									<option value="A+">A+</option>
+									<option value="A-">A-</option>
+									<option value="B+">B+</option>
+									<option value="B-">B-</option>
+									<option value="AB+">AB+</option>
+									<option value="AB-">AB-</option>
+									<option value="O+">O+</option>
+									<option value="O-">O-</option>
+
+								</select>
+							</div>
+							<div class="form-group center-aligned">
+								<button type="submit" class="btn btn-lg btn-danger">Search</button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+			<!-- header ends -->
+
+			<!-- donate section -->
+			<div class="container-fluid red-background">
+				<div class="row">
+					<div class="col-md-12">
+						<h1 class="text-center"  style="color: white; font-weight: 700;padding: 10px 0 0 0;">Donate The Blood</h1>
+						<hr class="white-bar">
+						<p class="text-center pera-text">
+							It is said that blood is one of the most priceless gifts one can give to another. Blood is essential for a person to stay alive. Many times due to accidents or any other serious ailments, a person might require blood. And in those times, people who step up to donate their blood are real-life superheroes.
+
+							It is said that blood is one of the most priceless gifts one can give to another. Blood is essential for a person to stay alive. Many times due to accidents or any other serious ailments, a person might require blood. And in those times, people who step up to donate their blood are real-life superheroes.
+						</p>
+						<a href="#" class="btn btn-default btn-lg text-center center-aligned">Become a Life Saver!</a>
+					</div>
+				</div>
+			</div>
+			<!-- end doante section -->
+			
+
+			<div class="container">
+				<div class="row">
+    				<div class="col">
+    					<div class="card">
+     						<h3 class="text-center red">Our Vission</h3>
+								<img src="img/binoculars.png" alt="Our Vission" class="img img-responsive" width="168" height="168">
+								<p class="text-center">
+									It is said that blood is one of the most priceless gifts one can give to another. Blood is essential for a person to stay alive. Many times due to accidents or any other serious ailments, a person might require blood. And in those times, people who step up to donate their blood are real-life superheroes.
+								</p>
+						</div>
+    				</div>
+    				
+    				<div class="col">
+    					<div class="card">
+      							<h3 class="text-center red">Our Goal</h3>
+								<img src="img/target.png" alt="Our Vission" class="img img-responsive" width="168" height="168">
+								<p class="text-center">
+									It is said that blood is one of the most priceless gifts one can give to another. Blood is essential for a person to stay alive. Many times due to accidents or any other serious ailments, a person might require blood. And in those times, people who step up to donate their blood are real-life superheroes.
+								</p>
+						</div>
+    				</div>
+    			
+    				<div class="col">
+    					<div class="card">
+      						<h3 class="text-center red">Our Mission</h3>
+								<img src="img/goal.png" alt="Our Vission" class="img img-responsive" width="168" height="168">
+								<p class="text-center">
+									It is said that blood is one of the most priceless gifts one can give to another. Blood is essential for a person to stay alive. Many times due to accidents or any other serious ailments, a person might require blood. And in those times, people who step up to donate their blood are real-life superheroes.
+								</p>
+							</div>
+   			 		</div>
+ 			</div>
+ 		</div>
+
+			<!-- end aboutus section -->
+
+
+<?php
+//include footer file
+include ('include/footer.php');
+ ?>
